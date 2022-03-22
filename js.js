324e7ba06d6df9e9a8ce8140741d5b2f8d28b9a4
unknown <camilaag2705@gmail.com>
2022-03-22 17:09:05 -0500

    Nuevo cine

--- js.js
+++ js.js
@@ -1,8 +1,35 @@
-let contenedor=document.getElementById("contenedor")
-contenedor.addEventListener("click",function(evento){
+let contenedor = document.getElementById("contenedor")
+contenedor.addEventListener("click", function (evento) {
 
-    if(evento.target.classList.contains("img-fluid")){
-        evento.target.src="img/cinema2.png"
+    if (evento.target.classList.contains("img-fluid")) {
+        evento.target.src = "img/cinema2.png"
     }
- 
+
 })
+
+let boton = document.getElementById("Boton")
+boton.addEventListener("click", escuchar)
+function escuchar(event) {
+    event.preventDefault()
+    console.log("hizo click")
+    let pelicula = document.getElementById("Pelicula").value;
+    console.log(pelicula)
+    let cantidad = document.getElementById("Cantidad").value;
+    console.log(cantidad)
+    let asiento = document.getElementById("Asiento").value;
+    console.log(asiento)
+    let correo = document.getElementById("Correo").value;
+    console.log(correo)
+
+    let preferencial = 12500;
+    let general = 9000;
+    console.log(asiento)
+    if (asiento == "Preferencial") {
+        let resultado = cantidad * preferencial
+        alert("Su total es: " + resultado)
+    }
+    else (asiento == "General")
+    let resultado2 = cantidad * general
+    alert("Su total es: " + resultado2)
+}
+
